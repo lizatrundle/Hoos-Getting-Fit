206d06250cc9a6dd7e4ad03af1ef386d282cb1f1
Liza Trundle <liza@solipay.tech>
2023-11-29 18:06:59 -0500

    adding delete acount

--- home.php
+++ home.php
@@ -9,32 +9,61 @@
     <meta name="description" content="include some description about your page">
     <title> DashBoard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      >
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh">
 </head>
+
 <body style="background-color: lightorange;">
 
 <?php 
-
 
 $email = array_key_exists('email', $_COOKIE) ? $_COOKIE['email'] : 'email not found in cookie';
 
 setrawcookie('email', $_COOKIE['email'] = $email);
 
+require('workoutdb.php');
+
+// Check if the delete account button is clicked
+if (isset($_POST['deleteAccount'])) {
+    $test = deleteUser($email);
+    echo $test;
+    if ($test == $email) {
+        header("Location: delete.php");
+        exit();
+    }
+    header("Location: login.php");
+    // exit();
+}
+
 ?>  
-  <div class="container">
+
+<div class="container">
     <h1>Welcome, <font color="green" style="font-style:italic"><?php echo $email; ?></font></h1>
     <p>
-      This is your dahsboard 
+        This is your dashboard.
     </p>
    
     <p>
-      Log workout <a href="logworkout.php" title="Start the first question"> log workout</a>.
-      <br/><br/>
-      Or <a href="logout.php">log out</a>.     
+        Log workout <a href="logworkout.php" title="Start the first question"> log workout</a>.
+        <br/><br/>
+        <form method="post" action="">
+            <input type="submit" name="logout" value="Log out" class="btn btn-warning" />
+            <br/><br/>
+            <input type="submit" name="deleteAccount" value="Delete Account" class="btn btn-danger" />
+        </form>
     </p>
 
+</div>
+
+<?php
+
+// Check if the logout button is clicked
+if (isset($_POST['logout'])) {
+   
+    header("Location: logout.php");
+    exit();
+}
+
+?>
     
-  </div>    
 </body>
 </html>
