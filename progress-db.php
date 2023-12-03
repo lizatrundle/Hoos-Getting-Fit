f81e7fbd6612d1b2e85292945918e7e2508f779a
juliamuratore <jrm4jw@virginia.edu>
2023-12-02 21:11:40 -0500

    progress-db edits

--- progress-db.php
+++ progress-db.php
@@ -1,1 +1,28 @@
-//todo
+<?php
+function logProgress($metric_id, $weight_change, $muscle_change, $new_nutrients, $heart_rate_change, $BMI_change, $email)
+{
+    global $db;
+    $query = "insert into loggedProgress values (:goalID, :weightChange, :muscleChange, :newNutrients, :hrChange, :bmiChange, :email)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':metricID',  $metric_id);
+    $statement->bindValue(':weightChange',  $weight_change);
+    $statement->bindValue(':muscleGain',  $muscle_change);
+    $statement->bindValue(':newNutrients',  $new_nutrients);
+    $statement->bindValue(':hrChange',  $heart_rate_change);
+    $statement->bindValue(':bmiChange',  $BMI_change);
+    $statement->bindValue(':email',  $email);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function getAllProgressMetrics($email) {
+    global $db;
+    $query="select * from loggedProgress where email=:email";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':email',  $email);
+    $statement->execute();
+    $result=$statement->fetchAll();
+    $statement->closeCursor();
+    return $result;
+}
+?> 
